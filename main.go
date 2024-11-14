ff867fbfe1f6cb4bc959c2adb7b5456ce2abd037
cnyegun <buichinguyen1105@gmail.com>
2024-11-14 12:42:20 +0200

    flow control

--- main.go
+++ main.go
@@ -1,21 +1,72 @@
 package main
 
 import (
+	// "fmt"
+	"fmt"
 	def "goreor/packages"
 )
 
 func main() {
-	mytask := def.Task{
-		Priority: "High",
-		Due:      "24-11-2024",
-		Added:    "13-11-2024",
-		Content:  "Go to LIDL buy meat",
-	}
-	def.PrintTask(mytask)
+	home_menu_loop()
 }
 
 func home_menu_loop() {
-	for {
+	labels := []def.Label{
+		{
+			Name: "Work",
+			Tasks: []def.Task{
+				{Id: 1, Priority: "Urgent", Due: "15-11-2024", Added: "01-11-2024", Content: "Finish report"},
+				{Id: 2, Priority: "High", Due: "20-11-2024", Added: "05-11-2024", Content: "Prepare presentation"},
+			},
+		},
+		{
+			Name: "Personal",
+			Tasks: []def.Task{
+				{Id: 3, Priority: "Low", Due: "25-12-2024", Added: "10-11-2024", Content: "Buy gifts"},
+				{Id: 4, Priority: "High", Due: "22-11-2024", Added: "12-11-2024", Content: "Book tickets"},
+			},
+		},
+		{
+			Name: "Fitness",
+			Tasks: []def.Task{
+				{Id: 5, Priority: "Urgent", Due: "15-11-2024", Added: "10-11-2024", Content: "Renew gym membership"},
+				{Id: 6, Priority: "Low", Due: "20-11-2024", Added: "05-11-2024", Content: "Buy new running shoes"},
+			},
+		},
+		{
+			Name: "Finance",
+			Tasks: []def.Task{
+				{Id: 7, Priority: "Low", Due: "25-12-2024", Added: "20-6-2024", Content: "Pay student loan"},
+				{Id: 8, Priority: "High", Due: "02-01-2025", Added: "21-6-2024", Content: "Buy more BTC"},
+			},
+		},
+	}
 
+	var cmd string
+	var opt string
+	var opt2 string
+	def.PrintHelp()
+	for {
+		opt = ""
+		cmd = ""
+		opt2 = ""
+
+		fmt.Print("=go.reor> ")
+		fmt.Scanln(&cmd, &opt, &opt2)
+
+		switch cmd {
+		case "ls":
+			def.ListLabels(labels)
+		case "see":
+			def.SeeLabel(labels, opt)
+		case "cl":
+			def.NewLabel(labels, opt)
+		case "rm":
+			def.RemoveLabel(labels, opt)
+		case "cn":
+			def.ChangeName(labels, opt, opt2)
+		case "ct":
+			def.CreateTask(labels, opt)
+		}
 	}
 }
--- packages/def.go
+++ packages/def.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Task struct {
+	Id int
 	// Priority is one of {"Urgent", "High","Low"}
 	Priority string
 	// Due and Added is date in format "DD-MM-yyyy"
@@ -14,14 +15,94 @@ type Task struct {
 }
 
 type Label struct {
-	Name string
-	Task []Task
+	Name  string
+	Tasks []Task
 }
 
-func PrintTask(t Task) {
-	fmt.Println("@Task INFO:")
-	fmt.Println(" >Priority:", t.Priority)
-	fmt.Println(" >Added   :", t.Added)
-	fmt.Println(" >Due     :", t.Due)
-	fmt.Println(" >Content :", t.Content)
+func printTask(t Task) {
+	fmt.Println("@Task ID   :", t.Id)
+	fmt.Println(" - Priority:", t.Priority)
+	fmt.Println(" - Added   :", t.Added)
+	fmt.Println(" - Due     :", t.Due)
+	fmt.Println(" - Content :", t.Content)
+	fmt.Println()
+}
+
+func PrintHelp() {
+	fmt.Println(" _____  __ _  ___   _ __ ___  ___  _ __")
+	fmt.Println("|_____|/ _` |/ _ \\ | '__/ _ \\/ _ \\| '__|")
+	fmt.Println("|_____| (_| | (_) || | |  __/ (_) | |")
+	fmt.Println("       \\__, |\\___(_)_|  \\___|\\___/|_|")
+	fmt.Println("       |___/                           ")
+	fmt.Println("============Available Commands============")
+	fmt.Println("> ls")
+	fmt.Println(" ?list all the labels")
+	fmt.Println()
+	fmt.Println("> see <label_name>")
+	fmt.Println(" ?list all the tasks in that label")
+	fmt.Println()
+	fmt.Println("> cl <label_name>")
+	fmt.Println(" ?create new label")
+	fmt.Println()
+	fmt.Println("> rm <label_name>")
+	fmt.Println(" ?remove a label")
+	fmt.Println()
+	fmt.Println("> cn <label_name> <new_name>")
+	fmt.Println(" ?change a label's name")
+	fmt.Println()
+	fmt.Println("> ct <label_name>")
+	fmt.Println(" ?create a task in that label")
+	fmt.Println("==========================================")
+
+}
+
+// []Label -> Stdout
+// Print the name of all label in stdout
+func ListLabels(l []Label) {
+	length := len(l)
+	fmt.Print("Labels: ")
+	for i := 0; i < length; i++ {
+		fmt.Printf("[%s] ", l[i].Name)
+	}
+	fmt.Println()
+}
+
+// []Label String -> Stdout
+// Search for label name and print all the label's task in stdout if matched
+func SeeLabel(l []Label, name string) {
+	length := len(l)
+	for i := 0; i < length; i++ {
+		if l[i].Name == name {
+			printTasks(l[i])
+			return
+		}
+	}
+	fmt.Printf("Can't find label:%s\n", name)
+}
+
+func printTasks(l Label) {
+	length := len(l.Tasks)
+	for i := 0; i < length; i++ {
+		printTask(l.Tasks[i])
+	}
+}
+
+// []Label String -> []Label
+// Create a new Label and put in []Label
+func NewLabel(l []Label, name string) {
+}
+
+// []Label String -> []Label
+// Remove a Label from []Label
+func RemoveLabel(l []Label, name string) {
+}
+
+// []Label String String -> []Label
+// Search for the matching label and change the name of it
+func ChangeName(l []Label, name string, new_name string) {
+}
+
+// []Label String -> []Label
+// Create a task inside the given Label name String
+func CreateTask(l []Label, name string) {
 }
